39bfe1d7c5fbb726236b670ab920eb267a7610fe
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-06 09:08:38 -0400

    callback aggregation example

---
+++ examples/aggregate.js
@@ -0,0 +1,24 @@
+var getAllPages = require('..'); 
+
+var env = process.env;
+var opts = {
+    uri: 'https://api.github.com/users/substack/repos'
+  , json: true
+  , body: { }
+  , headers: { 'user-agent': 'request-all-pages' } 
+  }
+  , startPage = 1
+  , pagesPer = 100;
+
+getAllPages(opts, startPage, pagesPer, function (err, pages) {
+  if (err) return console.error(err);  
+  var names = pages
+    .reduce(
+      function (acc, page) {
+        acc = acc.concat(page.body.map(function (repo) { return repo.name; }))
+        return acc;
+      }
+    , []);
+
+  console.log('%s\nTotal: %s', names.join(', '), names.length);
+});
