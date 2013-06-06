56ab84e14c7b06b1058089c17f8feef5505f51b0
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-06 09:03:49 -0400

    following pages works mostly, but not getting last x items if x is smaller than perPage

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 'use strict';
 
-// pagesize
-// [ headers]|header -- if array distribute the across reqs for each page
-// [ bodys]|body -- if array distribute the across reqs for each page
-//
+var request          =  require('request')
+  , xtendUrl         =  require('extend-url')
+  , queryLinkHeader  =  require('./lib/query-link-header')
+  , withPagingParams =  require('./lib/with-paging-params')
+  ;
 
-var qs = require('querystring')
-  , extend = require('util')._extend
-  , through = require('through');
+function nextPage(opts, cb) {
+  request(opts, function (err, res, body) {
+    if (err) return cb(err);  
+    if (/^[45]\d\d/.test(res.statusCode)) return cb(body);
+    cb(null, { headers: res.headers, body: body });
+  });
+}
 
+function getPages (opts, current, acc, cb) {
+  nextPage(opts, function (err, res) {
+    if (err) return cb(err);
+    
+    var links = queryLinkHeader(res.headers.link);
 
-var opts = {
-    uri: 'https://api.github.com/users/thlorenz/repos/?somevalue=1&someothervalue=2' 
-  , json: true
-  , body: { }
-  , headers: {} 
+    if (current >= links.last.page) return cb(null, acc); 
+
+    opts.uri = xtendUrl(opts.uri, links.next.link);
+    acc.push(res);
+
+    process.nextTick(getPages.bind(null, opts, links.next.page, acc, cb));
+  });
+}
+
+module.exports = function (opts, startPage, perPage, cb) {
+  opts.uri = withPagingParams(opts.uri, startPage, perPage);
+  return getPages(opts, startPage, [], cb)
 };
-
