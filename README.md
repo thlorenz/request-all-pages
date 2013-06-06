7560181604e570812e7ce414435d3efc9667b15f
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-05 21:21:20 -0400

    querying weblink properly

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Aggregates paged data for a request into one by traversing all pages.
 
 Follows the [link headers](http://tools.ietf.org/html/rfc5988) until it reaches the last page.
+[![build status](https://secure.travis-ci.org/thlorenz/request-all-pages.png)](http://travis-ci.org/thlorenz/request-all-pages)
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+
+
+
+var link = '<https://api.github.com/user/9287/repos?client_id=579929e71dadf664d8b8&client_secret=57544da13aa74556daecbda8ac15bcfb9d85f380&page=2&per_page=500>; rel="next", <https://api.github.com/user/9287/repos?client_id=579929e71dadf664d8b8&client_secret=57544da13aa74556daecbda8ac15bcfb9d85f380&page=3&per_page=500>; rel="last"'
