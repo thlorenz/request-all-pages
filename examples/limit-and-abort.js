b8798bdd2926271a0a67c7fe69263577f30fba77
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-24 08:00:15 -0400

    adding examples to demonstrate limit feature

---
+++ examples/limit-and-abort.js
@@ -0,0 +1,25 @@
+var requestAllPages = require('..')
+  , through = require('through');
+
+var requestOpts = {
+    uri: 'https://api.github.com/users/substack/repos'
+  , json: true
+  , body: {}
+  , headers: { 'user-agent': 'request-all-pages' } 
+  }
+  , pagesPer = 100;
+
+// aborts immediately since last page > maxPages
+requestAllPages(
+      requestOpts
+    , { pagesPer:  pagesPer, limit: { maxPages: 2, abort: true }  }
+  )
+  .on('error', console.error) 
+  .pipe(through(
+    function (data) {
+      var page = JSON.parse(data)
+        , names = page.body.map(function (repo) { return repo.name; });
+      this.queue(names.join(', '));
+    }
+  ))
+  .pipe(process.stdout);
---
+++ examples/limit-no-abort.js
@@ -0,0 +1,25 @@
+var requestAllPages = require('..')
+  , through = require('through');
+
+var requestOpts = {
+    uri: 'https://api.github.com/users/substack/repos'
+  , json: true
+  , body: {}
+  , headers: { 'user-agent': 'request-all-pages' } 
+  }
+  , pagesPer = 100;
+
+// gets only the first 2 pages
+requestAllPages(
+      requestOpts
+    , { pagesPer:  pagesPer, limit: { maxPages: 2, abort: false }  }
+  )
+  .on('error', console.error) 
+  .pipe(through(
+    function (data) {
+      var page = JSON.parse(data)
+        , names = page.body.map(function (repo) { return repo.name; });
+      this.queue(names.join(', '));
+    }
+  ))
+  .pipe(process.stdout);
